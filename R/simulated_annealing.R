aeb625ae4c1e574d71ce5d0de1698c75947525f2
Derek <derek.j.kruszewski@gmail.com>
2020-03-06 16:30:00 -0800

    add code for sim anneal

---
+++ R/simulated_annealing.R
@@ -0,0 +1,64 @@
+#' Feature selector that performs simmulated annealing to select features.
+#' 
+#' @description Algorithm randomly chooses a set of features, trains on them, scores the model.
+#'Then the algorithm slightly modifies the chosen features randomly and tests to see
+#'if the model improves. If there is improvement, the newer model is kept, if not the 
+#'algorithm tests to see if the worse model is still kept based on a acceptance 
+#'probability that decreases as iterations continue and if the model performs worse.
+#' 
+#' @param scorer  A custom user-supplied function that accepts X and y (as defined below) as input and returns the error of the datasets.
+#' @param X data frame of training features
+#' @param y data frame of training targets
+#' @param c control rate of feature perturbation
+#' @param iterations number of iterations
+#' @param bools If true function returns array of boolean values instead of column indicies
+#' 
+#' @return array of selected features
+#' @export
+#' 
+simulated_annealing <- function(model, X, y, c = 1, iterations = 100, bools = FALSE) {
+    
+    # Set mutate percentage
+    mutate <- 0.05
+    
+    # Obtain initial array of randomly selected features
+    ftr_all <- c(1:length(X))
+    ftr_old <- c()
+    while (length(ftr_old) == 0){
+        ftr_old <- (rbinom(length(X), 1, 0.5) != 0)
+    }
+    score_old <- scorer(cbind(X[ftr_old], y))
+    
+    # Iterate through new versions of selected features
+    for (i in 1:iterations){
+        ftr_new <- ftr_old
+        ftr_mutate <- sample(c(1:length(X)), size = ceiling(length(ftr_all)*mutate), replace = FALSE)
+        for (f in ftr_mutate) {
+            ftr_new[f] <- !ftr_new[f]
+        }
+        # Make sure new selected features has a least one feature
+        if (sum(ftr_new) != 0){
+            score_new <- scorer(cbind(X[ftr_new], y))
+            if (score_new < score_old){
+                ftr_old <- ftr_new
+                score_old <- score_new
+            } else {
+                # Determine probability of acceptance
+                p_accept <- exp((-i/c)*((score_new - score_old)/score_old))
+                if (runif(1) > p_accept){
+                } else {
+                    ftr_old <- ftr_new
+                    score_old <- score_new
+                }
+            }
+        }
+    }
+    
+    # Return either feature indicies or booleans
+    if (bools){
+        return(ftr_old)
+    } else {
+        return(ftr_all[ftr_old])
+    }
+}
+
