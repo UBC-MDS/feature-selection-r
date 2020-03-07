1cb49e394360f75534435771b6c55670057cba40
Ryan Homer <ryan.homer@alumni.ubc.ca>
2020-03-07 00:48:57 -0800

    RFE implementation with first test

--- R/recursive_feature_elimination.R
+++ R/recursive_feature_elimination.R
@@ -9,8 +9,7 @@
 #' negative of that score, for example negative MSE.
 #'
 #' @param scorer A user-defined function that fits an estimnator and returns a score.
-#' @param X features
-#' @param y response
+#' @param data A data.frame or tibble
 #' @param n_features_to_select The target number of features.
 #'
 #' @return A vector of feature indices representing selected features in ranked order.
@@ -18,6 +17,24 @@
 #'
 #' @examples
 #' # TBA
-recursive_feature_elimination <- function(scorer, X, y, n_features_to_select=None) {
-  c()
+recursive_feature_elimination <- function(scorer, data, n_features_to_select) {
+  eliminated_features <- c()
+  total_features <- ncol(data) - 1
+
+  for (i in 1:(ncol(data) - 2)) {
+    # Remove currently eliminated features
+    features_to_try <- dplyr::select(data, -eliminated_features)
+
+    # Get the next feature to remove
+    feature_to_remove <- scorer(features_to_try)
+    eliminated_features <- c(eliminated_features, feature_to_remove)
+
+    # If we have our target number of features, stop.
+    if (length(eliminated_features) + n_features_to_select >= total_features)
+      break
+  }
+
+  # Return a vector of the features to keep
+  purrr::reduce(names(data),
+                function(acc, col) { if (any(eliminated_features == col)) acc else c(acc, col) })
 }
--- tests/testthat/test-recursive_feature_elimination.R
+++ tests/testthat/test-recursive_feature_elimination.R
@@ -1,8 +1,13 @@
-scorer <- function() {
+set.seed(0)
+df <- tgp::friedman.1.data()
 
+scorer <- function(data) {
+  model <- lm(Y ~ ., data)
+  names(which.min(model$coefficients[-1]))[[1]]
 }
 
 test_that("it works", {
-  features <- recursive_feature_elimination(scorer, X, y, n_features_to_select = 4)
-  expect_identical(features, c())
+  data <- dplyr::select(df, -Ytrue)
+  features <- recursive_feature_elimination(scorer, data, n_features_to_select = 4)
+  expect_identical(features, c("X1", "X2", "X4", "X5", "Y"))
 })
