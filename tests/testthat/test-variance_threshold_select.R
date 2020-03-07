a62147771cd749b977bb29d176ab8606e66cb4b7
vault <jackyho112dev@gmail.com>
2020-03-07 13:01:31 -0800

    Add variance thresholdin

---
+++ tests/testthat/test-variance_threshold_select.R
@@ -0,0 +1,43 @@
+#
+# Tests for variance_threshold_select()
+#
+
+testthat::test_that("relevant features remain", {
+  data <- tibble(
+    a = c(1,2,3),
+    b = c(2,2,2),
+    c = c('123', '123', '123'),
+    d = c('123', '124', '1')
+  )
+
+  feature_indexes <- variance_threshold_select(data)
+  testthat::expect_identical(feature_indexes, c(1, 4))
+})
+
+testthat::test_that("higher threshold", {
+  data <- tibble(
+    a = c(1,2,3),
+    b = c(2,2,2),
+    c = c('123', '123', '123'),
+    d = c('123', '14', '1')
+  )
+
+  feature_indexes <- variance_threshold_select(data, threshold = 2)
+  testthat::expect_identical(feature_indexes, c(4))
+})
+
+testthat::test_that("inputs checking", {
+  data <- tibble(
+    a = c(1,2,3),
+    d = c('123', '124', '1')
+  )
+
+  testthat::expect_error(
+    recursive_feature_elimination(data, threshold=-1),
+    "Threshold must be a positive number."
+  )
+  testthat::expect_error(
+    recursive_feature_elimination(123, threshold=-1),
+    "Expected a `data.frame` object for `data`."
+  )
+})
